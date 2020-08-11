a5959c52835113b81c44a54e3ef3009c4955595a
bigdata1 <tsbslteisrhy@naver.com>
2020-08-11 13:29:19 +0900

    관리자 메인 페이지, 주문완료 페이지 실습 추가

---
+++ kmarket/js/best.js
@@ -0,0 +1,16 @@
+$(function(){
+    var best = $('aside > .best');
+    $(window).scroll(function(){
+        var t = $(this).scrollTop();
+        
+        if(t > 620) {
+            best.css({
+                'position': 'fixed',
+                'top': '0'
+                });
+        }else{
+            best.css({'position':'static'})
+        }
+
+    });
+});
