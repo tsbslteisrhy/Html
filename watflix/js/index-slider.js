e26d653b98a1f51a32dff881e80ba4cf2bed3b77
USER <tsbslteisrhy@naver.com>
2020-09-07 21:16:21 +0900

    watflix index 작업

---
+++ watflix/js/index-slider.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $('.keyvisual-slider .bxslider').bxSlider({
+        pager: false,
+        auto: true,
+        pause: 3000
+    });
+});
+
+$(document).ready(function(){
+    $('.thumb-slider').bxSlider({
+        pager: false
+    });
+});
