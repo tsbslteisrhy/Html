b2f23fe7b4c5b5d418ebc2d254606d9228213b45
bigdata1 <tsbslteisrhy@naver.com>
2020-09-08 12:51:38 +0900

    vod list 작업중

---
+++ watflix/js/slider.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    $('#index-main .keyvisual-slider .bxslider').bxSlider({
+        pager: false,
+        auto: true,
+        pause: 3000
+    });
+});
+
+$(document).ready(function(){
+    $('#index-main .thumb-slider').bxSlider({
+        pager: false
+    });
+});
+
+$(document).ready(function(){
+    $('#vod-main .recommend-slider').bxSlider({
+        pager: false
+    });
+});
